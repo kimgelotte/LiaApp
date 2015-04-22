70d5a58923e81d2899bcc467859ac2aa366fc9fe
johan-w <johan.westerlund@live.com>
2015-04-22 14:53:14 +0200

    connection sträng funkar

--- LiaApp/Program.cs
+++ LiaApp/Program.cs
@@ -14,9 +14,12 @@ namespace LiaApp
         [STAThread]
         static void Main()
         {
+            AzureCon blabla = new AzureCon();
+            blabla.azureconnect();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            
         }
     }
 }

5d35d45bea6f395b307971b386b1bc93a6838a41
Amorious <ramco.amro@hotmail.com>
2015-05-05 14:10:40 +0200

    Amro

--- LiaApp/Program.cs
+++ LiaApp/Program.cs
@@ -16,7 +16,7 @@ namespace LiaApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run( new StartApp());
         }
     }
 }
---
+++ LiaApp/StartApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LiaApp
+{
+    public partial class StartApp : Form
+    {
+       
+        public StartApp()
+        {
+            InitializeComponent();
+           
+        }
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}

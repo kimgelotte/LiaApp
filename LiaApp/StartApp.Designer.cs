bdb42df04bd89ef63e89d7c57f707d4d8fe5db08
kimgelotte <me@kimgelotte.se>
2015-05-19 14:28:47 +0200

    Lite pill

--- LiaApp/StartApp.Designer.cs
+++ LiaApp/StartApp.Designer.cs
@@ -52,14 +52,14 @@
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
-            "Personal",
-            "Elev"});
+            "Staff",
+            "Student"});
+            this.comboBox1.SelectedIndex = 1;
             this.comboBox1.Location = new System.Drawing.Point(236, 128);
             this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(136, 21);
             this.comboBox1.TabIndex = 2;
-            this.comboBox1.SelectedIndex = 0;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // textBox1
